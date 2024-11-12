6d16545fd059ba0a3e03ae74f7dfefb3845d158f
Your Name <your.email@example.com>
2024-11-12 15:37:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+      
+ $('.owl-carousel').owlCarousel({
+  loop: true,
+  width:241,
+  margin:20,
+ 
+ 
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 5
+    },
+    600: {
+      items: 5
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
+ 
